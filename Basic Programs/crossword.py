57abbdc1ef2a6f3e2ab1eba89ad7018a4ce1bfc8
drishtantr <drishtantregmi777@gmail.com>
2020-07-01 11:47:55 +0545

    Crossword Puzzle

---
+++ Basic Programs/crossword.py
@@ -0,0 +1,8 @@
+import random
+alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# puz=np.zeros( (23, 23) )
+print(random.choice(alpha))
+# for i in range(23):
+#     for j in range(23):
+#         print(random.choice(alpha))
+
