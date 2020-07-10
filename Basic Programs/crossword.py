35ef92a719856394194dcc013669a90a7a17385d
drishtantr <drishtantregmi777@gmail.com>
2020-07-10 13:50:48 +0545

    Puzzle added

--- Basic Programs/crossword.py
+++ Basic Programs/crossword.py
@@ -1,8 +1,11 @@
 import random
 alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # puz=np.zeros( (23, 23) )
+puz=[]
 print(random.choice(alpha))
-# for i in range(23):
-#     for j in range(23):
-#         print(random.choice(alpha))
+for i in range(3):
+    for j in range(3):
+        puz.append(random.choice(alpha))
+        # puz[i][j]=random.choice(alpha)
+print(puz)
 
